59f2ed0c297cc4ceb226da2f203dc4c563cecb4b
Samuel-Lopes-projetos <samuellopesdossantosneto@gmail.com>
2026-09-04 17:07:24 -0300

    commit

--- Assets/Scripts/PlayerTwo.cs
+++ Assets/Scripts/PlayerTwo.cs
@@ -3,28 +3,53 @@ using UnityEngine;
 public class PlayerTwo : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
+    private Animator anim;
     public Rigidbody2D rb;
+
     private Vector2 movement;
 
     void Start()
     {
         GameManager.Gameover = false;
+
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if(!GameManager.Gameover){
-
+        if (!GameManager.Gameover)
+        {
             movement.x = Input.GetAxisRaw("Horizontal");
             movement.y = Input.GetAxisRaw("Vertical");
 
-            movement.Normalize(); // evita movimento mais rápido na diagonal
+            movement.Normalize();
+
+            if (movement.x < 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else if (movement.x > 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
         }
-        else{
-            moveSpeed = 0;
+
+        else
+        {
+            movement = Vector2.zero;
         }
+
+        if (movement.x < 0 || movement.y < 0 || movement.x > 0 || movement.y > 0) {
+            anim.SetBool("andando", true);
+
+        }
+
+        else
+        {
+            anim.SetBool("andando", false);
+        }
+
     }
     void FixedUpdate()
     {
